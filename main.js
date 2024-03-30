3d2a2e6e8f28d67774aad0a257be5e9cefdfcf6d
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2024-03-30 14:33:19 -0300

    commit
    adding my portfolio on my github

---
+++ main.js
@@ -0,0 +1,33 @@
+function getProjects() {
+  const urlGithub = 'https://api.github.com/users/MarcusCSPereira/repos';
+  var loadingElement = document.getElementById('loading');
+
+  fetch(urlGithub, {
+    method: 'GET'
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      loadingElement.style.display = 'none';
+      showProjects(response);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+}
+
+function showProjects(data) {
+  var listElement = document.getElementById('my-projects-list');
+  for (let i = 0; i < data.length; i++) {
+    let a = document.createElement("a");
+    a.href = data[i]['clone_url'];
+    a.target = "_blank";
+    a.title = data[i]['description'];
+    if(data[i]['name'] != 'MarcusCSPereira' && data[i]['name'] != ''){
+      let linkText = document.createTextNode(data[i]['name']);
+      a.appendChild(linkText);
+      listElement.appendChild(a);
+    }
+}
+}
+
+getProjects();
